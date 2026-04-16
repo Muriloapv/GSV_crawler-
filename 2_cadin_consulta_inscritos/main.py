9f60982393666a14ab5d7cd92044f705fb21aa18
Muriloapv <muriloalves121@outlook.com>
2026-04-16 11:48:08 -0300

    update 2, 3

--- 2_cadin_consulta_inscritos/main.py
+++ 2_cadin_consulta_inscritos/main.py
@@ -4,37 +4,39 @@ import re
 import os
 import ddddocr
 import json
+from datetime import datetime
 
 cpfCnpj        = '11462916988'
 url            = 'https://www3.prefeitura.sp.gov.br/cadin/Pesq_Deb.aspx' 
 caminhoCaptcha = r"C:\Temp\captcha.jpg"
    
+#Pega a imagem do captcha e a trata
 def getCaptcha( _captchaImg ) -> str:
-    _captchaImg.wait_for( state="visible" )
-    _captchaImg.screenshot( path=caminhoCaptcha )
+   _captchaImg.wait_for( state="visible" )
+   _captchaImg.screenshot( path=caminhoCaptcha )
 
-    imagem          = Image.open(caminhoCaptcha)
-    largura, altura = imagem.size
+   imagem          = Image.open(caminhoCaptcha)
+   largura, altura = imagem.size
 
-    imagem          = imagem.resize((largura * 3, altura * 3), Image.LANCZOS)
-    imagem          = imagem.convert("L")
-    imagem.save(caminhoCaptcha)
+   imagem          = imagem.resize((largura * 3, altura * 3), Image.LANCZOS)
+   imagem          = imagem.convert("L")
+   imagem.save(caminhoCaptcha)
     
-    returnCaptcha   = ler_captcha(caminhoCaptcha)
-    print( f"Captacha: '{returnCaptcha}'" )
-    return returnCaptcha 
+   returnCaptcha   = ler_captcha(caminhoCaptcha)
+   print( f"Captacha: '{returnCaptcha}'" )
+   return returnCaptcha 
 
+#Faz a leitura do captcha de img para texto
 def ler_captcha( caminho: str ) -> str:
-    ocr = ddddocr.DdddOcr( show_ad=False )
+   ocr = ddddocr.DdddOcr( show_ad=False )
 
-    with open(caminho, "rb") as f:
-       imagem_bytes = f.read()
+   with open(caminho, "rb") as f:
+      imagem_bytes = f.read()
 
-    texto = ocr.classification(imagem_bytes)
-    return re.sub(r'[^A-Za-z0-9]', '', texto).upper()
+   texto = ocr.classification(imagem_bytes)
+   return re.sub(r'[^A-Za-z0-9]', '', texto).upper()
    
 with sync_playwright() as pw:
-   # navegador = pw.chromium.launch(headless=False)
     navegador   = pw.chromium.launch_persistent_context(
         user_data_dir=r'C:\Temp\pw_profile',
         headless=False,#alterar para true caso não queira que abra a aba
@@ -58,11 +60,10 @@ with sync_playwright() as pw:
     if btnCookies.count() > 0:
         btnCookies.click()#avaliar negar
 
-    print('1')
     textoCaptcha = getCaptcha(captchaImg)
     pagina.get_by_role("textbox", name="CNPJ ou CPF.").click()
     pagina.wait_for_timeout(500)
-    pagina.get_by_role("textbox", name="CNPJ ou CPF.").type( cpfCnpj.upper(), delay=120 )
+    pagina.get_by_role("textbox", name="CNPJ ou CPF.").type( cpfCnpj, delay=120 )
     pagina.wait_for_timeout(800)
     pagina.mouse.move(300, 400)
 
@@ -77,10 +78,11 @@ with sync_playwright() as pw:
     json_path      = os.path.join(BASE_DIR, "cadin_consulta.json" )
     resultPesquisa = pagina.inner_text("span#lbl_NaoAchou").strip()
     dados_json = {
-        "url"      : url,
-        "pagina"   : paginaTitle,
-        "cpf_cnpj" : cpfCnpj,
-        "result"   : resultPesquisa    
+        "url"          : url,
+        "pagina"       : paginaTitle,
+        "cpf_cnpj"     : cpfCnpj,
+        "result"       : resultPesquisa,
+        "consultado_em": datetime.now().strftime("%Y-%m-%d %H:%M:%S")    
     }
 
     with open( json_path, 'w', encoding='utf-8') as f:
--- 3_cadin_cadastro/main.py
+++ 3_cadin_cadastro/main.py
@@ -1,2 +1,98 @@
 from playwright.sync_api import sync_playwright
+from PIL import Image
+import re
+import ddddocr
+from datetime import datetime
 
+cpfCnpj        = '11462916988'
+nome           = 'Murilo Alves Pereira Varoto'
+dtNascimento   = '07/01/2004'
+url            = 'https://cadin.prefeitura.sp.gov.br/FiscRecFed.aspx'
+caminhoCaptcha = r"C:\Temp\captcha.jpg"
+max_tentativas = 3
+   
+#Pega a imagem do captcha e a trata
+def getCaptcha( _captchaImg ) -> str:
+   _captchaImg.wait_for( state="visible" )
+   _captchaImg.screenshot( path=caminhoCaptcha )
+
+   imagem          = Image.open(caminhoCaptcha)
+   largura, altura = imagem.size
+
+   imagem          = imagem.resize((largura * 3, altura * 3), Image.LANCZOS)
+   imagem          = imagem.convert("L")
+   imagem          = imagem.point(lambda x: 0 if x < 140 else 255)
+   imagem.save(caminhoCaptcha)
+    
+   returnCaptcha   = ler_captcha(caminhoCaptcha)
+   print( f"Captacha: '{returnCaptcha}'" )
+   return returnCaptcha 
+
+#Faz a leitura do captcha de img para texto
+def ler_captcha( caminho: str ) -> str:
+   ocr = ddddocr.DdddOcr( show_ad=False )
+
+   with open(caminho, "rb") as f:
+      imagem_bytes = f.read()
+
+   texto = ocr.classification(imagem_bytes)
+   return re.sub(r'[^A-Za-z0-9]', '', texto).upper()
+   
+def inputCaptcha( pagina ) -> str:
+   pagina.wait_for_timeout(3000)
+   pagina.mouse.move(200, 300)
+   
+   captchaImg   = pagina.get_by_role("img", name="Imagem")
+   textoCaptcha = getCaptcha(captchaImg)
+   return textoCaptcha
+
+with sync_playwright() as pw:
+   navegador = pw.chromium.launch_persistent_context(
+      user_data_dir=r'C:\Temp\pw_profile',
+      headless=False,#alterar para true caso não queira que abra a aba
+      channel="chrome", 
+      ignore_default_args=["--enable-automation"], 
+      args=[
+            "--disable-blink-features=AutomationControlled",
+            "--start-maximized",
+            "--no-sandbox",
+         ]
+    )   
+   pagina       = navegador.new_page()
+   pagina.goto( url )
+   paginaTitle  = pagina.title()
+   textoCaptcha = inputCaptcha( pagina )
+
+   pagina.get_by_role("textbox", name="CPF. CPF deve ser digitado").type( cpfCnpj, delay=120 )
+   pagina.wait_for_timeout(800)
+   pagina.get_by_role("textbox", name="Nome. Campo deve ser digitado").type( nome, delay=120 )
+   pagina.wait_for_timeout(500)
+   pagina.get_by_role("textbox", name="Data de Nascimento. Data deve").type( dtNascimento, delay=120 )
+    
+   pagina.mouse.move(300, 400)
+   campoCaptcha = pagina.locator("#txtwcpSenha")
+
+   campoCaptcha.click()
+   campoCaptcha.type(textoCaptcha, delay=120)
+
+   pagina.wait_for_timeout(800)
+   pagina.mouse.move(300, 400)     
+   pagina.get_by_role("button", name="Login").click()
+
+   if pagina.url == url:
+      mensagem = 'Não foi possivel efetuar login'
+      for tentativas in range( 1, max_tentativas +1 ):
+         textoCaptcha = inputCaptcha( pagina )
+         campoCaptcha.fill("")
+         campoCaptcha.type(textoCaptcha, delay=120)
+         pagina.get_by_role("button", name="Login").click()
+
+         if pagina.url != url:
+            mensagem = 'Login efetuado com sucesso'
+            break
+
+   print( paginaTitle )
+   print( mensagem )
+   pagina.wait_for_timeout(5000)
+   navegador.close()
+  
